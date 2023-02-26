1d18f855b1af1b37ff89965a848fe43bc2b1e2b0
ProsperFall <74441642+ProsperFall@users.noreply.github.com>
2023-02-26 16:25:24 +0800

    Update bootx64.c

--- bootx64/bootx64.c
+++ bootx64/bootx64.c
@@ -8,12 +8,6 @@
 #include <Protocol/DevicePathToText.h>
 #include <Protocol/LoadedImage.h>
 
-typedef struct {
-  UINT8 Type;
-  UINT8 SubType; 
-  UINT16 Length;
-} Device_Path;
-
 extern EFI_BOOT_SERVICES *gBS;
 EFI_STATUS Status;
 
@@ -23,98 +17,16 @@ UINTN MapKey = 0;
 UINTN DscSiz = 0;
 UINT32 DscVer = 0;
 
-EFI_STATUS EFIAPI SetUpThisDisk()
-{
-  Print(L"QAQ");
-  Exit(Status);
-  return EFI_SUCCESS;
-}
-
 EFI_STATUS EFIAPI UefiMain (  IN EFI_HANDLE ImageHandle, IN EFI_SYSTEM_TABLE  *SystemTable )
 {
-  EFI_LOADED_IMAGE* ImageInfo;//ImageInfo->DeviceHandle
-  Status = gBS -> HandleProtocol(ImageHandle,&gEfiLoadedImageProtocolGuid,(VOID**)&ImageInfo);
-  if(EFI_ERROR(Status))
-  {
-    SystemTable->ConOut->OutputString(SystemTable->ConOut,L"Boot Device not found\n");
-    Exit(Status);
-  }
-  
-  EFI_FILE_IO_INTERFACE* Volume;//Access ESP Volume
-  Status = gBS -> HandleProtocol(ImageInfo->DeviceHandle,&gEfiSimpleFileSystemProtocolGuid,(VOID**)&Volume);
-  if(EFI_ERROR(Status))
-  {
-    SystemTable->ConOut->OutputString(SystemTable->ConOut,L"Destroyed disk!\n");
-    Exit(Status);
-  }
-
-  EFI_FILE_HANDLE FileSystemRootPath;
-  Volume->OpenVolume(Volume,&FileSystemRootPath);
-  
-  EFI_FILE_HANDLE BootCacheFile;
-  Status = FileSystemRootPath->Open(FileSystemRootPath,(EFI_FILE**)&BootCacheFile,L".iTache_",EFI_FILE_MODE_READ|EFI_FILE_MODE_WRITE|EFI_FILE_MODE_CREATE,0);
-  //While the file does not exist Creat it.
-  if(EFI_ERROR(Status))
-  {
-    SystemTable->ConOut->OutputString(SystemTable->ConOut,L"While booting Error Occur.\n");
-    Exit(Status);
-  }
-  
-  UINTN BufferSize = sizeof(EFI_FILE_INFO) + sizeof(CHAR16) * 100;
-  EFI_FILE_INFO* FileInfo;
-  Status = gBS->AllocatePool(EfiRuntimeServicesData,BufferSize,(VOID**)&FileInfo);
-  if(EFI_ERROR(Status))
-  {
-    SystemTable->ConOut->OutputString(SystemTable->ConOut,L"Memory Allocate Error Occured.\n");
-    Exit(Status);
-  } 
-
-  BootCacheFile->GetInfo(BootCacheFile,&gEfiFileInfoGuid,&BufferSize,FileInfo);
-  BufferSize = FileInfo->FileSize;
-
-ReadAgain:
-  Status = BootCacheFile->Read(BootCacheFile,&BufferSize,(VOID*)0x80000000);//bootloader
-
-  //EFI_DEVICE_PATH* DevicePath;
-  HARDDRIVE_DEVICE_PATH* DevicePath;
-  DevicePath = (HARDDRIVE_DEVICE_PATH*)0x80000000;
-
-  EFI_DEVICE_PATH_TO_TEXT_PROTOCOL* DevicePathChecker;
-  Status = gBS->LocateProtocol(&gEfiDevicePathToTextProtocolGuid,NULL,(VOID**)&DevicePathChecker);
-  if(DevicePathChecker->ConvertDevicePathToText(DevicePath,TRUE,FALSE) == NULL)
-  {
-    SetUpThisDisk();
-  }
-
-  EFI_HANDLE DeviceHandle;
-  Status = gBS -> LocateDevicePath(&gEfiBlockIoProtocolGuid,(EFI_DEVICE_PATH**)&DevicePath,&DeviceHandle);//means locate by DevPath
-  if(EFI_ERROR(Status))
-  {
-    SetUpThisDisk();
-  }
-  //Open BlockProtocol
-  EFI_BLOCK_IO_PROTOCOL* BlockIO;
-  Status = gBS -> HandleProtocol(DeviceHandle,&gEfiBlockIoProtocolGuid,(VOID**)&BlockIO);
-  if(EFI_ERROR(Status))
-  {
-    SystemTable->ConOut->OutputString(SystemTable->ConOut,L"Device not support BlockIO.\n");
-    Exit(Status);
-  }
-UINT8* BootLoader;
-  BootLoader = (UINT8*)0x80000000;//2,147,483,648 Bytes 2GB
-  Status = BlockIO->ReadBlocks(
-    BlockIO,
-    BlockIO->Media->MediaId,
-    0,//LBA
-    BlockIO->Media->BlockSize,
-    (VOID**)BootLoader
-  );
-  for(int n = 0; n < 4096 ; n++)
-  {
-    Print(L"%x ",*BootLoader);
-    BootLoader++;
-  }
-  //Get Memory Map
+  //get boot device PCI BDF(0:0:0)
+  //load BCD（BootConfigurationData）
+  //load basic fileSystem drive
+  //send read kenrel instruction to media
+  //report to kenrel SystemTable and MemoryMapping
+  UINT8* BootLoader;
+  BootLoader = (UINT8*)0x80000000;//set kenrel base address
+  //Free
   gBS->GetMemoryMap(&MemMapSiz,MemMap,&MapKey,&DscSiz,&DscVer);//get length
   gBS->AllocatePool(EfiReservedMemoryType,MemMapSiz,(VOID**)&MemMap);
   gBS->GetMemoryMap(&MemMapSiz,MemMap,&MapKey,&DscSiz,&DscVer);//save msg
@@ -126,9 +38,9 @@ UINT8* BootLoader;
     SystemTable->ConOut->OutputString(SystemTable->ConOut,L"Invaid Memory Map.\n");
     Exit(EFI_INVALID_PARAMETER);
   }
+  //call Kenrel
   void (*bootloader)(void);
   bootloader = (void(*)(void))0x80000000;
   bootloader();
-  goto ReadAgain;
   return EFI_SUCCESS;
 }
