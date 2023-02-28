5b9afc542f92457616557f1ce8dbc4b16bda89cc
ProsperFall <74441642+ProsperFall@users.noreply.github.com>
2023-02-28 15:32:45 +0800

    Update bootx64.c

--- bootx64/bootx64.c
+++ bootx64/bootx64.c
@@ -24,21 +24,43 @@ EFI_STATUS EFIAPI UefiMain (  IN EFI_HANDLE ImageHandle, IN EFI_SYSTEM_TABLE  *S
   //load basic fileSystem drive
   //send read kenrel instruction to media
   //report to kenrel SystemTable and MemoryMapping
-  UINT8* BootLoader;
-  BootLoader = (UINT8*)0x80000000;//set kenrel base address
-  //Free
+  EFI_LOADED_IMAGE* ImageInfo;
+  Status = gBS -> HandleProtocol(ImageHandle,&gEfiLoadedImageProtocolGuid,(VOID**)&ImageInfo);
+  
+  EFI_FILE_IO_INTERFACE* Volume;//Access ESP Volume
+  Status = gBS -> HandleProtocol(ImageInfo->DeviceHandle,&gEfiSimpleFileSystemProtocolGuid,(VOID**)&Volume);
+
+  EFI_FILE_HANDLE FileSystemRootPath;
+  Volume->OpenVolume(Volume,&FileSystemRootPath);
+  EFI_FILE_HANDLE BootConfigData;
+  Status = FileSystemRootPath->Open(FileSystemRootPath,(EFI_FILE**)&BootConfigData,L"BCD",EFI_FILE_MODE_READ/*|EFI_FILE_MODE_WRITE|EFI_FILE_MODE_CREATE*/,0);
+  
+  UINTN BufferSize = sizeof(EFI_FILE_INFO) + sizeof(CHAR16) * 100;
+  EFI_FILE_INFO* FileInfo;
+  Status = gBS->AllocatePool(EfiRuntimeServicesData,BufferSize,(VOID**)&FileInfo);
+
+  BootConfigData->GetInfo(BootConfigData,&gEfiFileInfoGuid,&BufferSize,FileInfo);
+  CHAR8* LoadedBootConfigData;
+  Status = gBS->AllocatePool(EfiRuntimeServicesData,FileInfo->FileSize,(VOID**)&LoadedBootConfigData);
+
+  Status = BootConfigData->Read(BootConfigData,&BufferSize,(VOID*)&LoadedBootConfigData);
+
+  EFI_DEVICE_PATH_TO_TEXT_PROTOCOL* DevicePathChecker;
+
+  EFI_HANDLE DeviceHandle;
+
+  //Open BlockProtocol
+  EFI_BLOCK_IO_PROTOCOL* BlockIO;
+  Status = gBS -> HandleProtocol(DeviceHandle,&gEfiBlockIoProtocolGuid,(VOID**)&BlockIO);
+
+  //Get Memory Map
   gBS->GetMemoryMap(&MemMapSiz,MemMap,&MapKey,&DscSiz,&DscVer);//get length
   gBS->AllocatePool(EfiReservedMemoryType,MemMapSiz,(VOID**)&MemMap);
   gBS->GetMemoryMap(&MemMapSiz,MemMap,&MapKey,&DscSiz,&DscVer);//save msg
   gBS->FreePool(MemMap);
   gBS->GetMemoryMap(&MemMapSiz,MemMap,&MapKey,&DscSiz,&DscVer);//fresh MemMap
   Status = gBS->ExitBootServices(ImageHandle,MapKey);
-  if(EFI_ERROR(Status))
-  {
-    SystemTable->ConOut->OutputString(SystemTable->ConOut,L"Invaid Memory Map.\n");
-    Exit(EFI_INVALID_PARAMETER);
-  }
-  //call Kenrel
+
   void (*bootloader)(void);
   bootloader = (void(*)(void))0x80000000;
   bootloader();
