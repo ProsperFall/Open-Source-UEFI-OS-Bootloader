695951334b8742538dfeb0a314422f91f10ba18a
ProsperFall <74441642+ProsperFall@users.noreply.github.com>
2023-03-01 12:26:35 +0800

    Create Bootloader.h

---
+++ Bootloader/Bootloader.h
@@ -0,0 +1,73 @@
+typedef struct __BootConfigration
+{
+    CHAR8 TYPE;
+    /*
+        1110 0000B:OSU-BL BCD Head
+        1110 0001B:Open an UEFI_APPLICATION with an UefiMain ENTRY_POINT
+        0000 0001B:Boot an system with path of an Volume with File system
+        0000 0000B:None Useable BCD
+        OTHER:Reserved
+    */
+    CHAR8 PARTITION;
+    /*
+        0~127, Boot an system without Open another UEFI_APPLICATION, THIS CAN'T BE '0'
+        <0 Unuseable Boot Configration
+    */
+    CHAR8 LANG;
+    /*
+        VersionSpec
+    */
+    CHAR8 VERSION;
+    CHAR8 TIMEOUT;/*Seconds*/
+    CHAR8 METHOD;/*Reserved*/
+    CHAR8 NUMBER;/*Pointer/64*/
+    INT16 _Rsvd_;
+} BootConfigration;
+
+typedef struct __BootConfigration_EFI  //TYPE 1110 0000B
+{
+    BootConfigration Head;
+    CHAR8 BootCongigrationCount;//useable Count,the Count must 128
+    CHAR8 _Rsvd_0;
+    CHAR8 ParameterRequirements;
+    /*
+      1--- ----,SystemTable
+      -1-- ----,ImageHandle
+      ---- ---1,MemoryInfo
+      ---1 ----,ExtParameter
+    */
+    CHAR8 _Rsvd_1;
+    CHAR16 _Rsvd_2;
+    INT32 SystemPathPointer;//(CHAR8*)
+    INT32 DescriptionPointer;
+    INT32 GUID_POINTER;
+    INT32 _Rsvd_;
+} BootConfigration_EFI;
+
+typedef struct __BootConfigration_APP  //TYPE 1110 0001B
+{
+    BootConfigration Head;
+    CHAR8 _Rsvd_0;
+    CHAR8 ExternParameterPtrA8bit;
+    CHAR8 ParameterRequirements;
+    CHAR8 ExternParameterPtrB8bit;
+    CHAR16 ExternParameterPtrC16bit;
+    INT32 SystemPathPointer;//(CHAR8*)
+    INT32 DescriptionPointer;
+    INT32 GUID_POINTER;
+    INT32 _Rsvd_;
+} BootConfigration_APP;
+
+typedef struct __BootConfigration_SYS  //TYPE 0000 0001B
+{
+    BootConfigration Head;
+    CHAR8 _Rsvd_0;
+    CHAR8 ExternParameterPtrA8bit;//low8bit
+    CHAR8 ParameterRequirements;
+    CHAR8 ExternParameterPtrB8bit;//high8bit of low16bit
+    CHAR16 ExternParameterPtrC16bit;//high16bit
+    INT32 SystemPathPointer;//(CHAR8*)
+    INT32 DescriptionPointer;
+    INT32 GUID_POINTER;
+    INT32 _Rsvd_;
+} BootConfigration_SYS;
