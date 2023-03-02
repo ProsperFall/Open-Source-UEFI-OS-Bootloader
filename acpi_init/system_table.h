2c971c309f908ff389a4831a54caa48897f121c0
ProsperFall <74441642+ProsperFall@users.noreply.github.com>
2023-03-02 11:19:47 +0800

    Update system_table.h

--- acpi_init/system_table.h
+++ acpi_init/system_table.h
@@ -1,1 +1,14 @@
 #include <stdint.h>
+
+typedef struct __Root_system_description_ptr__
+{
+  char Signature[8];
+  uint8_t Checksum;
+  char OEMID[6];
+  uint8_t Revision;
+  uint32_t RsdtAddr;//32 bit physical address of the RSDT.
+  uint32_t Length;
+  uint64_t XsdtAddr;
+  uint8_t ExtendedChecksum;
+  uint8_t __Rsvd[3]
+} RSDP;
