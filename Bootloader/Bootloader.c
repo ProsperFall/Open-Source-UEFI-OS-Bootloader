d930c26a2d1967cca92ad5a0a932e5f012bb80da
ProsperFall <74441642+ProsperFall@users.noreply.github.com>
2023-03-06 16:04:06 +0800

    Update Bootloader.c

--- Bootloader/Bootloader.c
+++ Bootloader/Bootloader.c
@@ -9,6 +9,7 @@
 #include <Bootloader.h>
 #include <FileSystem.h>
 #include <acpi_init/system_table.h>
+#include <acpi_init/pcie/pci_config.h>
 
 EFI_STATUS Status;
 extern EFI_BOOT_SERVICES* gBS;
@@ -40,17 +41,25 @@ EFI_STATUS EFIAPI UefiMain ( IN EFI_HANDLE ImageHandle, IN EFI_SYSTEM_TABLE *Sys
   INT32 EntryCount = (ExtSysDecTable->Header.Length-36)>>3;
   Print(L"EntryCount: %d\n",EntryCount);
   UINT64* EntryPtr = (UINT64*)ExtSysDecTable->Entry;
-
+  BOOLEAN FoundMcfg = FALSE;
   for(UINTN Index = 0;Index < EntryCount;Index++,EntryPtr++)
   {
-    if(AcpiTableSignIs(*EntryPtr,&McfgSign)) break;
+    if(AcpiTableSignIs(*EntryPtr,&McfgSign))
+    {
+      FoundMcfg = TRUE;
+      break;
+    }
+  }
+  if(!FoundMcfg)
+  {
+    return EFI_UNSUPPORTED;
   }
   Print(L"%lx: ",*EntryPtr);
   Print(L"%lx\n",*(UINT64*)*EntryPtr);
   MCFG* Mcfg = (MCFG*)*EntryPtr;
-  Print(L"0x%016lx%\n",(UINT64)*(UINT64*)Mcfg->BaseAddressOfEcam);//BaseAddressOfEcam
-  //load acpi table
-
+  Print(L"0x%016lx%\n",(UINT64)*(UINT64*)Mcfg->BaseAddressOfEcam);
+  INT32 EcamCount = (Mcfg->Header.Length-44)>>4;
+  ECAM* EcamPool = (ECAM*)Mcfg->BaseAddressOfEcam;
 
   EFI_LOADED_IMAGE* ImageInfo = NULL;
   Status = gBS->HandleProtocol(ImageHandle,&gEfiLoadedImageProtocolGuid,(VOID**)&ImageInfo);
@@ -59,26 +68,41 @@ EFI_STATUS EFIAPI UefiMain ( IN EFI_HANDLE ImageHandle, IN EFI_SYSTEM_TABLE *Sys
   ACPI_HID_DEVICE_PATH* PCI_ROOT = (ACPI_HID_DEVICE_PATH*)BootDevicePath;
   INT32 PCI_BUS = PCI_ROOT->UID;
   *(INT16*)&BootDevicePath += (UINT64)(UINT16)*BootDevicePath->Length;  //*(INT16*)& cancel displacement caused by cast.
-  //EFI_DEVICE_PATH* BootDeviceRootNode = BootDevicePath; //save node
-  PCI_DEVICE_PATH* PCI_BF = (PCI_DEVICE_PATH*)BootDevicePath;
-  INT32 PCI_DEV =  PCI_BF->Device;
-  INT32 PCI_FUN =  PCI_BF->Function;
-  INT64 PCI_BDF_DISP = (PCI_BUS<<20)|(PCI_DEV<<15)|(PCI_FUN<<12);  //PCI Root
+  PCI_DEVICE_PATH* PCI_BF;
+  INT32 PCI_DEV;
+  INT32 PCI_FUN;
+  INT64 PCI_BDF_DISP;
   INT32 PciNodeCount = 0;
+  PciConfigSpaceBridge* PciConfig1 = NULL;
   do
   {
+    PCI_BF = (PCI_DEVICE_PATH*)BootDevicePath;
+    PCI_DEV =  PCI_BF->Device;
+    PCI_FUN =  PCI_BF->Function;
+    PCI_BDF_DISP = (PCI_BUS<<20)|(PCI_DEV<<15)|(PCI_FUN<<12);
+    for(UINTN Index =0; Index<EcamCount; Index++)
+    {
+      if((EcamPool[Index].StartPciBusNum <= PCI_BUS) && (PCI_BUS<=EcamPool[Index].EndPciBusNum))
+      {
+        PciConfig1 = (UINT64)EcamPool[Index].BassAdressOfEcam;
+        Print(L"ecam::0x%016lx\n",(UINT64)EcamPool[Index].BassAdressOfEcam);
+        PciConfig1 = (UINT64)PciConfig1 | PCI_BDF_DISP;
+        Print(L"PciConfigAddr:0x%016lx\n",PciConfig1);
+        PCI_BUS = PciConfig1->SubordinateBusNumber;
+        break;
+      }
+    }
     *(INT16*)&BootDevicePath += (UINT64)(UINT16)*BootDevicePath->Length;  //next node
     PciNodeCount++;
-    /*
-      record next bus;
-    */
   } while ((BootDevicePath->Type==HARDWARE_DEVICE_PATH) && (BootDevicePath->SubType==HW_PCI_DP));
-  Print(L"PCI node count: %d\n",PciNodeCount);
+  //get Disk BDF
 
+  Print(L"PCI node count: %d\n",PciNodeCount);
+  //Print(L"PCI Head Type: %d\n",PciConfig1->HeadType);
   if((BootDevicePath->Type==MESSAGING_DEVICE_PATH)&&(BootDevicePath->SubType==MSG_NVME_NAMESPACE_DP))
   {
     //NVME_NAMESPACE_DEVICE_PATH* NvmDevicePath = (NVME_NAMESPACE_DEVICE_PATH*)&BootDevicePath;
-    Print(L"Yes!Yes!Yes!\n");
+    Print(L"Nvm!\n");
   }
   else if(0)
   {
