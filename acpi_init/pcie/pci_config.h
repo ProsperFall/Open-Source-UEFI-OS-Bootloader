64132bf778534cd46cdcae35c7bf8b1e0902a6df
ProsperFall <74441642+ProsperFall@users.noreply.github.com>
2023-03-18 19:32:21 +0800

    Update pci_config.h

--- acpi_init/pcie/pci_config.h
+++ acpi_init/pcie/pci_config.h
@@ -4,32 +4,30 @@
 
 typedef struct _pci_config_spce_
 {
-  uint16_t DeviceID;
   uint16_t VendorID;
-  uint16_t Status;
+  uint16_t DeviceID;
   uint16_t Command;
-  uint16_t ClassCodeLower16bits;//only little endian(x86)
-  uint8_t ClassCodeUpper8bits;//
+  uint16_t Status;
   uint8_t RevisionID;
-  uint8_t BIST;
-  uint8_t HeadType;
-  uint8_t LatencyTimer;
+  uint8_t ClassCode[3];
   uint8_t CacheLineSize;
+  uint8_t LatencyTimer;
+  uint8_t HeadType;
+  uint8_t BIST;
 } PciConfigSpaceHead;
 
 typedef struct _pci_config_head_type_0_
 {
-  uint16_t DeviceID;
   uint16_t VendorID;
-  uint16_t Status;
+  uint16_t DeviceID;
   uint16_t Command;
-  uint16_t ClassCodeLower16bits;//only little endian(x86)
-  uint8_t ClassCodeUpper8bits;//
+  uint16_t Status;
   uint8_t RevisionID;
-  uint8_t BIST;
-  uint8_t HeadType;
-  uint8_t LatencyTimer;
+  uint8_t ClassCode[3];
   uint8_t CacheLineSize;
+  uint8_t LatencyTimer;
+  uint8_t HeadType;
+  uint8_t BIST;
   
   uint32_t BaseAddressRegister_1;
   uint32_t BaseAddressRegister_2;
@@ -38,55 +36,57 @@ typedef struct _pci_config_head_type_0_
   uint32_t BaseAddressRegister_5;
   uint32_t BaseAddressRegister_6;
   uint32_t CardbusCISPtr;
-  uint16_t SubsystemID;
   uint16_t SubsystemVendorID;
+  uint16_t SubsystemID;
+  
   uint32_t ExpansionROMBaseAddress;
-  uint16_t _Rsvd_;
-  uint8_t _Rsvd1_;
+  
   uint8_t CapabilitiesPtr;
-  uint32_t _Rsvd2_;
-  uint8_t MAX_Lat;
-  uint8_t Min_Gnt;
-  uint8_t InterruptPin;
+  uint8_t _Rsvd_[7];
   uint8_t InterruptLine;
+  uint8_t InterruptPin;
+  uint8_t Min_Gnt;
+  uint8_t MAX_Lat;
+
+
+
 } PciConfigSpaceDevice;
 
 typedef struct _pci_config_head_type_1_
 {
-  uint16_t DeviceID;
   uint16_t VendorID;
-  uint16_t Status;
+  uint16_t DeviceID;
   uint16_t Command;
-  uint16_t ClassCodeLower16bits;//only little endian(x86)
-  uint8_t ClassCodeUpper8bits;//
+  uint16_t Status;
   uint8_t RevisionID;
-  uint8_t BIST;
-  uint8_t HeadType;
-  uint8_t LatencyTimer;
+  uint8_t ClassCode[3];
   uint8_t CacheLineSize;
+  uint8_t LatencyTimer;
+  uint8_t HeadType;
+  uint8_t BIST;
   
   uint32_t BaseAddressRegister_1;
   uint32_t BaseAddressRegister_2;
-  uint8_t SecondaryLatencyTimer;
-  uint8_t SubordinateBusNumber;
-  uint8_t SecondaryBusNumber;
   uint8_t PrimaryBusNumber;
-  uint16_t SecondaryStatus;
-  uint8_t IOLimit;
+  uint8_t SecondaryBusNumber;
+  uint8_t SubordinateBusNumber;
+  uint8_t SecondaryLatencyTimer;
   uint8_t IOBase;
-  uint16_t MemoryLimit;
+  uint8_t IOLimit;
+  uint16_t SecondaryStatus;
   uint16_t MemoryBase;
-  uint16_t PrefetchableMemoryLimit;
+  uint16_t MemoryLimit;
   uint16_t PrefetchableMemoryBase;
+  uint16_t PrefetchableMemoryLimit;
+
   uint32_t PrefetchableBaseUpper32Bits;
   uint32_t PrefetchableLimitUpper32Bits;
-  uint16_t IOBaseLimit16Bits;
   uint16_t IOBaseUpper16Bits;
-  uint16_t _Rsvd_;
-  uint8_t _Rsvd1_;
+  uint16_t IOBaseLimit16Bits;
   uint8_t CapabilitiesPtr;
-  uint32_t ExpansionROMBaseAddress;
-  uint16_t BridgeControl;
-  uint8_t InterruptPin;
+  uint8_t _Rsvd_[3];
   uint8_t InterruptLine;
+  uint8_t InterruptPin;
+  uint16_t BridgeControl;
+  uint32_t ExpansionROMBaseAddress;
 } PciConfigSpaceBridge;
