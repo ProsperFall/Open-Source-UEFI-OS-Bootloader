a8eb77bfe8956056baf271f5a141fa59af1cb552
ProsperFall <74441642+ProsperFall@users.noreply.github.com>
2024-08-13 07:56:51 +0800

    uploade

---
+++ main/main.c
@@ -0,0 +1,11 @@
+#include <efi.h>
+#include <efilib.h>
+
+EFI_STATUS Status = EFI_SUCCESS;
+
+extern EFI_BOOT_SERVICES *gBS;
+
+EFI_STATUS EFIAPI efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
+{
+    return EFI_SUCCESS;
+}
